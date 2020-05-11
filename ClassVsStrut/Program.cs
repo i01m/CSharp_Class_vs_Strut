3dab4db39397573485dbac740ee77ee1873ff5e3
unknown <i.p.m.a.n.03.31.87@gmail.com>
2020-05-11 16:40:51 -0500

    first commint

---
+++ ClassVsStrut/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ClassVsStrut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = 7;
+            Console.WriteLine("Initial strut value is {0}",n);
+            Console.WriteLine();
+
+            //example - the whole value of strut is copied/passed into the method Increment,but not being returned
+            //since strut is VALUE type
+            Increment(n);
+            Console.WriteLine("example (doesn't increment) - Strut value from the Increment method is {0}", n);
+            Console.WriteLine();
+
+            //option1 - using word ref - only the reference to that strut is passed into the method Increment            
+            Increment(ref n);
+            Console.WriteLine("option 1 (using word 'ref') - Strut value from the Increment method is {0}", n);
+
+            //option2 - using word "out"
+            Increment(n, out n);
+            Console.WriteLine("option 2 (with word 'out') - Strut value from the Increment method is {0} ", n);
+
+
+            //option3 - 
+            n = IncrementWithReturn(n);
+            Console.WriteLine("option 3 (with int return method type) - Strut value from the IncrementWithReturn method is {0} ", n);
+
+            Console.ReadKey();
+
+        }
+        //example
+        //calculation happens,but once the method is finished, the result is lost(nothing is passing back)
+        static void Increment(int i)
+        {
+            i++;
+        }
+
+        //option1
+        //since only reference is passed in, calculation happens and the actual strut is changed
+        //reference is the pointer to the original strut value
+        static void Increment(ref int i)
+        {
+            i++;
+        }
+
+        //option2
+        //adding the word OUT
+        static void Increment(int n, out int n2)
+        {
+            n++;
+            n2 = n;
+        }
+
+        //option3 - changing return method type from void to int
+        static int IncrementWithReturn (int i)
+        {
+            i++;
+            return i;
+        }
+
+        
+    }
+}
---
+++ WorkingWithStruts/WorkingWithStruts/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using static System.Console;
+
+namespace WorkingWithStruts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Example 1:");
+            WriteLine();
+            int n = 7;
+            WriteLine($"Initial n value: {n}");
+            Increment(n,out n);
+            WriteLine($"Value n after method Increment(): {n}");
+            WriteLine();
+            WriteLine();
+
+
+            WriteLine("Example 2:");
+            WriteLine();
+            var p = new Point { X = 7, Y = 18 };
+            WriteLine($"Initial p values: {p.X},{p.Y}");
+            Clear(ref p);
+            WriteLine($"P values after method Clear(): {p.X},{p.Y}");
+            WriteLine();
+            WriteLine();
+
+
+            WriteLine("Example 3");
+            WriteLine();
+            DateTime date = DateTime.Now;
+            WriteLine($"Initial date: {date}");
+            DateTime newDate = MyAddHours(date, 3);
+            WriteLine($"Date after method AddHours(): {newDate}");
+
+
+            WriteLine();
+            WriteLine();
+            WriteLine("Press any key to continue...");
+            ReadKey();
+        }
+
+        //using the word "ref"
+        static void Clear(ref Point p)
+        {
+            p.X = p.Y = 0;
+        }
+
+        //using the word "return"
+        static DateTime MyAddHours(DateTime dt, int count)
+        {
+            DateTime newDt = dt.AddHours(count);
+            return newDt;
+        }
+
+        //using the word "out"
+        static void Increment(int i, out int i2)
+        {
+            i++;
+            i2 = i;
+        }
+             
+    }
+}
